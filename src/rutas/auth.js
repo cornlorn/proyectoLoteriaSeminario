050e967efd274939f4851eed654a76fd93c3c91f
Pablo Sosa <sosadavid4280@gmail.com>
2025-11-04 04:43:55 -0600

    elimina archivos innecesarios

--- src/rutas/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controladores/authControlador');
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
