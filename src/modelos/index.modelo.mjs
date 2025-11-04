b7db6c43d4a1ba605eac53c96c88af595bd1c29b
Pablo Sosa <sosadavid4280@gmail.com>
2025-11-04 04:29:45 -0600

    actualiza relaciones entre modelos

--- src/modelos/index.modelo.mjs
+++ src/modelos/index.modelo.mjs
@@ -1,11 +1,30 @@
 import { Billetera } from "./billetera.modelo.mjs";
 import { Cliente } from "./cliente.modelo.mjs";
+import { Transaccion } from "./transaccion.modelo.mjs";
 import { Usuario } from "./usuario.modelo.mjs";
 
-Usuario.hasOne(Cliente, { foreignKey: "usuario_id", as: "cliente" });
+Usuario.hasOne(Cliente, {
+  foreignKey: "usuario_id",
+  as: "cliente",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
 Cliente.belongsTo(Usuario, { foreignKey: "usuario_id", as: "usuario" });
 
-Cliente.hasOne(Billetera, { foreignKey: "cliente_id", as: "billetera" });
+Cliente.hasOne(Billetera, {
+  foreignKey: "cliente_id",
+  as: "billetera",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
 Billetera.belongsTo(Cliente, { foreignKey: "cliente_id", as: "cliente" });
 
-export { Cliente, Usuario, Billetera };
+Billetera.hasMany(Transaccion, {
+  foreignKey: "billetera_id",
+  as: "transacciones",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+Transaccion.belongsTo(Billetera, { foreignKey: "billetera_id", as: "billetera" });
+
+export { Cliente, Usuario, Billetera, Transaccion };
