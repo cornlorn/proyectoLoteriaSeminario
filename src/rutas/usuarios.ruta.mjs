b0cae618a4d0c81bc1cc8a05027adb958bf1da0b
Pablo Sosa <sosadavid4280@gmail.com>
2025-11-04 23:11:38 -0600

    agrega reenvío de tokens

--- src/rutas/usuarios.ruta.mjs
+++ src/rutas/usuarios.ruta.mjs
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { ingresar } from "../controladores/usuarios/ingresar.controlador.mjs";
+import { reenviarToken } from "../controladores/usuarios/reenviar-codigo.controlador.mjs";
 import { verificarCorreo } from "../controladores/usuarios/verificar-correo.controlador.mjs";
 import { validarIngreso } from "../validaciones/usuarios/ingreso.validacion.mjs";
+import { validarReenviarToken } from "../validaciones/usuarios/reenviar-token.validacion.mjs";
 import { validarVerificacionCorreo } from "../validaciones/usuarios/verificar-correo.validacion.mjs";
 
 const router = Router();
@@ -9,5 +11,7 @@ const router = Router();
 router.post("/ingresar", validarIngreso, ingresar);
 
 router.post("/verificar-correo", validarVerificacionCorreo, verificarCorreo);
+
+router.post("/reenviar-codigo", validarReenviarToken, reenviarToken);
 
 export { router as usuariosRutas };
