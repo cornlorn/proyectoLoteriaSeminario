f813d52aa1376f57f09d183a05606c0b5a3ea0e8
Pablo Sosa <sosadavid4280@gmail.com>
2025-11-04 22:01:41 -0600

    agrega verificación de token

--- src/rutas/usuarios.ruta.mjs
+++ src/rutas/usuarios.ruta.mjs
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { ingresar } from "../controladores/usuarios/ingresar.controlador.mjs";
+import { verificarCorreo } from "../controladores/usuarios/verificar-correo.controlador.mjs";
 import { validarIngreso } from "../validaciones/usuarios/ingreso.validacion.mjs";
+import { validarVerificacionCorreo } from "../validaciones/usuarios/verificar-correo.validacion.mjs";
 
 const router = Router();
 
 router.post("/ingresar", validarIngreso, ingresar);
+
+router.post("/verificar-correo", validarVerificacionCorreo, verificarCorreo);
 
 export { router as usuariosRutas };
