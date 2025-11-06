4d574c31d3b958f618325f0afa8d52964eb120db
Pablo Sosa <sosadavid4280@outlook.com>
2025-11-05 21:16:58 -0600

    actualiza tipo de id usado

--- src/modelos/cliente.modelo.mjs
+++ src/modelos/cliente.modelo.mjs
@@ -4,7 +4,7 @@ import { sequelize } from "../config/database.config.mjs";
 export const Cliente = sequelize.define(
   "Cliente",
   {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
     nombre: { type: DataTypes.STRING, allowNull: false },
     apellido: { type: DataTypes.STRING, allowNull: false },
     sexo: { type: DataTypes.ENUM("masculino", "femenino"), allowNull: false },
