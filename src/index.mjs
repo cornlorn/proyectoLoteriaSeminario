708e9bcc62754d36c3d3805258229be034b28ae4
Pablo Sosa <sosadavid4280@outlook.com>
2025-11-06 01:33:50 -0600

    agrega cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,4 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
@@ -8,6 +9,7 @@ import { rutas } from "./rutas/index.ruta.mjs";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
