b54dbd9a66373406754105b8ccacf8df7250f8a0
Pablo Sosa <sosadavid4280@outlook.com>
2025-11-06 02:15:31 -0600

    corrige ejemplo de correo en endpoints

--- src/modelos/index.modelo.mjs
+++ src/modelos/index.modelo.mjs
@@ -1,7 +1,5 @@
 import { Billetera } from "./billetera.modelo.mjs";
 import { Cliente } from "./cliente.modelo.mjs";
-import { Juego } from "./juego.modelo.mjs";
-import { Modalidad } from "./modalidad.modelo.mjs";
 import { Token } from "./token.modelo.mjs";
 import { Transaccion } from "./transaccion.modelo.mjs";
 import { Usuario } from "./usuario.modelo.mjs";
@@ -38,12 +36,4 @@ Billetera.hasMany(Transaccion, {
 });
 Transaccion.belongsTo(Billetera, { foreignKey: "billetera_id", as: "billetera" });
 
-Juego.hasMany(Modalidad, {
-  foreignKey: "juego_id",
-  as: "modalidades",
-  onDelete: "CASCADE",
-  onUpdate: "CASCADE",
-});
-Modalidad.belongsTo(Juego, { foreignKey: "juego_id", as: "juego" });
-
-export { Billetera, Cliente, Juego, Modalidad, Token, Transaccion, Usuario };
+export { Billetera, Cliente, Token, Transaccion, Usuario };
