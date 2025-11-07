f3863de1e7f30932773cc9151b23d4e821b425ce
Pablo Sosa <sosadavid4280@outlook.com>
2025-11-07 14:35:25 -0600

    corrige rutas de importación

--- src/rutas/auth/cambiar-contrasena.ruta.mjs
+++ src/rutas/auth/cambiar-contrasena.ruta.mjs
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { cambiarContrasena } from "../../controladores/clientes/cambiar-contrasena.controlador.mjs";
+import { cambiarContrasena } from "../../controladores/auth/cambiar-contrasena.controlador.mjs";
 import { proteger } from "../../middlewares/auth.middleware.mjs";
-import { validarCambiarContrasena } from "../../validaciones/clientes/cambiar-contrasena.validacion.mjs";
+import { validarCambiarContrasena } from "../../validaciones/auth/cambiar-contrasena.validacion.mjs";
 
 const router = Router();
 
